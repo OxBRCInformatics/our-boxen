fbc03179ec39f26463a1c1cffa41a35a8114ddfa
Ryan Brooks <ryanbrooksis@gmail.com>
2013-11-25 16:27:38 +0000

    Revert "Latest modules"
    This reverts commit 68884bd215a8da25221c94364d24c5999b62caf3.

--- modules/adobe_reader/spec/classes/adobe_reader_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'adobe_reader' do
-
-  it { should contain_class('adobe_reader') }
-  it { should contain_package('AdobeReader.pkg').with_provider('pkgdmg') }
-  it { should contain_package('AdobeReader.pkg').with_source('http://ardownload.adobe.com/pub/adobe/reader/mac/11.x/11.0.04/en_US/AdbeRdr11004_en_US.dmg') }
-
-end
