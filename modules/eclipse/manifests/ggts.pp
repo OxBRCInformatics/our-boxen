4d738edc49741bde8ed65e7dd2e22c3c26ec6d59
Charles Crichton <charles.crichton@ndm.ox.ac.uk>
2013-11-25 09:42:09 +0000

    Updated to latest URL for ggts

---
+++ modules/eclipse/manifests/ggts.pp
@@ -0,0 +1,12 @@
+# Public: Install Eclipse GGTS to /Applications
+#
+# Examples
+#
+#   include eclipse::ggts
+class eclipse::ggts {
+  package { 'Eclipse':
+    provider => 'compressed_app',
+    #source   => 'http://download.springsource.com/release/STS/3.3.0/dist/e4.3/groovy-grails-tool-suite-3.3.0.RELEASE-e4.3-macosx-cocoa-x86_64.tar.gz',
+    source	 => 'http://download.springsource.com/release/STS/3.4.0/dist/e4.3/groovy-grails-tool-suite-3.4.0.RELEASE-e4.3.1-macosx-cocoa-x86_64.tar.gz'
+  }
+}
